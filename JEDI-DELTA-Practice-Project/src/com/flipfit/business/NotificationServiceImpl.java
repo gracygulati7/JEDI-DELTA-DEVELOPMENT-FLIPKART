55679b7b431333a9084f2ac9c2b60520b9bf6ae8
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2026-01-23 15:42:29 +0530

    Add notification methods for booking and waitlist
    Implemented methods for sending booking confirmations and waitlist promotions.

--- JEDI-DELTA-Practice-Project/src/com/flipfit/business/NotificationServiceImpl.java
+++ JEDI-DELTA-Practice-Project/src/com/flipfit/business/NotificationServiceImpl.java
@@ -1,1 +1,11 @@
 package com.flipfit.business;
+
+public class NotificationServiceImpl implements NotificationService{
+    public void sendBookingConfirmation(int userId, int slotId) {
+        System.out.println("Notification: Booking confirmed for User " + userId + " at Slot " + slotId);
+    }
+
+    public void sendWaitlistPromotion(int userId, int slotId) {
+        System.out.println("Notification: You have moved from Waitlist to Confirmed!");
+    }
+}
