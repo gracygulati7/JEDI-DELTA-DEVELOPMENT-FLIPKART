259ab8bd9be2a60b2c05cdbc6ead6173b534c4ac
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2026-01-23 15:45:26 +0530

    Add getters and setters for Waitlist properties

--- JEDI-DELTA-Practice-Project/src/com/flipfit/bean/Waitlist.java
+++ JEDI-DELTA-Practice-Project/src/com/flipfit/bean/Waitlist.java
@@ -1,10 +1,35 @@
 package com.flipfit.bean;
 
 import java.util.Queue;
+import java.util.LinkedList;
 
 public class Waitlist {
 
-	private int waitlistId;
-	private int slotId;
-	private Queue<Integer> customerQueue;
+    private int waitlistId;
+    private int slotId;
+    private Queue<Integer> customerQueue = new LinkedList<>(); 
+
+    public int getWaitlistId() {
+        return waitlistId;
+    }
+
+    public void setWaitlistId(int waitlistId) {
+        this.waitlistId = waitlistId;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+
+    public Queue<Integer> getCustomerQueue() {
+        return customerQueue;
+    }
+
+    public void setCustomerQueue(Queue<Integer> customerQueue) {
+        this.customerQueue = customerQueue;
+    }
 }
