d828e3da326cd41d197d679e007597b2200d879a
samravisiva <168008290+samravisiva@users.noreply.github.com>
2026-01-27 15:40:12 +0530

    Add files via upload

---
+++ JEDI-DELTA-Practice-Project/src/com/flipfit/util/DBUtil.java
@@ -0,0 +1,24 @@
+package com.flipfit.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/FLipfit_Schema";
+    private static final String USER = "root"; // Replace with your MySQL 
+    private static final String PASS = "!Sam2004"; // Replace with your MySQL password
+
+    static {
+    	try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    	} catch (ClassNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    public static Connection getConnection() throws SQLException {
+    	return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+}
