e3240c247cd3bb10d8e6d410001f9239eb806de5
Adityarayyy <adityasray46@gmail.com>
2026-01-28 19:19:12 +0530

    added commentings

--- JEDI-DELTA-Practice-Project/src/com/flipfit/bean/FlipFitAdmin.java
+++ JEDI-DELTA-Practice-Project/src/com/flipfit/bean/FlipFitAdmin.java
@@ -1,5 +1,12 @@
 package com.flipfit.bean;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FlipFitAdmin.
+ *
+ * @author JEDI-DELTA
+ * @ClassName  "FlipFitAdmin"
+ */
 public class FlipFitAdmin extends FlipFitUser {
     
     public FlipFitAdmin() {
--- JEDI-DELTA-Practice-Project/src/com/flipfit/bean/FlipFitGymOwner.java
+++ JEDI-DELTA-Practice-Project/src/com/flipfit/bean/FlipFitGymOwner.java
@@ -1,5 +1,14 @@
 package com.flipfit.bean;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FlipFitGymOwner.
+ *
+ * Represents a gym owner with KYC details and approval state.
+ *
+ * @author JEDI-DELTA
+ * @ClassName  "FlipFitGymOwner"
+ */
 public class FlipFitGymOwner {
 
     private int ownerId;
@@ -9,6 +18,16 @@ public class FlipFitGymOwner {
     private String gstin;
     private boolean isValidated;
     private boolean isApproved;
+
+    /**
+     * Instantiates a new flip fit gym owner.
+     *
+     * @param ownerId the owner id
+     * @param name the owner's name
+     * @param pan the PAN identifier
+     * @param aadhaar the Aadhaar identifier
+     * @param gstin the GSTIN identifier
+     */
     public FlipFitGymOwner(int ownerId, String name, String pan, String aadhaar, String gstin) {
         this.ownerId = ownerId;
         this.name = name;
@@ -19,42 +38,92 @@ public class FlipFitGymOwner {
         this.isApproved = false; 
     }
 
+    /**
+     * Gets the owner id.
+     *
+     * @return the owner id
+     */
     public int getOwnerId() {
         return ownerId;
     }
 
+    /**
+     * Gets the owner's name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the PAN.
+     *
+     * @return the pan
+     */
     public String getPan() {
         return pan;
     }
 
+    /**
+     * Gets the Aadhaar.
+     *
+     * @return the aadhaar
+     */
     public String getAadhaar() {
         return aadhaar;
     }
 
+    /**
+     * Gets the GSTIN.
+     *
+     * @return the gstin
+     */
     public String getGstin() {
         return gstin;
     }
 
+    /**
+     * Checks if the owner is validated.
+     *
+     * @return true, if validated
+     */
     public boolean isValidated() {
         return isValidated;
     }
 
+    /**
+     * Sets the validated flag.
+     *
+     * @param validated the new validated status
+     */
     public void setValidated(boolean validated) {
         isValidated = validated;
     }
 
+    /**
+     * Checks if the owner is approved.
+     *
+     * @return true, if approved
+     */
     public boolean isApproved() {
         return isApproved;
     }
 
+    /**
+     * Sets the approved flag.
+     *
+     * @param approved the new approved status
+     */
     public void setApproved(boolean approved) {
         isApproved = approved;
     }
 
+    /**
+     * Returns a string representation of the gym owner.
+     *
+     * @return the string
+     */
     @Override
     public String toString() {
         return "GymOwner [OwnerId=" + ownerId + ", Name=" + name + ", Pan=" + pan + 
